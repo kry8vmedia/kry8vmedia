b534bc163d23834a3ce7102d9f33f78fe15147ba
Kry8v Media <kry8vmedia@gmail.com>
2016-11-30 15:17:46 +0200

    reorder of elements to reduce resource blocking

--- _assets/js/core.js
+++ _assets/js/core.js
@@ -1,3 +1,5 @@
+//=jquery.min
+//=bootstrap.min
 //=requirecssrelpreload.js
 //= require ie10-viewport-bug-workaround
 //= require typed.js
--- _assets/js/scripts.js
+++ _assets/js/scripts.js
@@ -1,23 +1,4 @@
+//= requirefont-awesome
 //= require smoothscroll
 //= require animations
 //= require cloudinary-jquery
-//cloudinary
-$.cloudinary.config({"api_key":"619824693495266","cloud_name":"krvmedia"});
-//Cloudinary Responsive
-$.cloudinary.responsive()
-//Analytics
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-  ga('create', 'UA-87431730-1', 'auto');
-  ga('send', 'pageview');
-  //Android Bug Workaround
-$(function () {
-  var nua = navigator.userAgent
-  var isAndroid = (nua.indexOf('Mozilla/5.0') > -1 && nua.indexOf('Android ') > -1 && nua.indexOf('AppleWebKit') > -1 && nua.indexOf('Chrome') === -1)
-  if (isAndroid) {
-    $('select.form-control').removeClass('form-control').css('width', '100%')
-  }
-})
